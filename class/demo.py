69fa17754b15c34622ed2b6c887e4b69ad53ef2f
Ranavrujal53 <csrana1213@gmail.com>
2025-12-24 10:23:27 +0530

    flie change

---
+++ class/demo.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+print('Hello')
+
+print(""" hsvc ccwubc
+      ecjbwcj
+      cbdwcw
+      ncndcn
+      wkbcw
+      lnicnw
+      """)
+
+print("hello'..wolrd")
+
+print('world " hello')
+
+print("hello ""world")
